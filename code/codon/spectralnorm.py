3433d64b664ce6b15512b4fd038a7d6b6e7fbb13
vnzstc <vnzstc@protonmail.com>
2024-11-17 11:08:52 +0100

    added spectral norm to codon set

--- code/codon/spectralnorm.py
+++ code/codon/spectralnorm.py
@@ -12,29 +12,35 @@ def eval_A(i, j):
 
 def eval_A_times_u(N, u, Au):
   for i in range(N):  
-    Au[i] = 0
+    Au[i] = 0.
     for j in range(N):
         Au[i] += eval_A(i,j) * u[j]    
 
 def eval_At_times_u(N, u, Au):
   for i in range(N):  
-    Au[i]=0
+    Au[i] = 0.
     for j in range(N): Au[i]+=eval_A(j,i)*u[j]   
 
 def eval_AtA_times_u(N, u, AtAu):
-  v=[0]*N; eval_A_times_u(N,u,v); eval_At_times_u(N,v,AtAu)
+  v=[0.]*N; eval_A_times_u(N,u,v); eval_At_times_u(N,v,AtAu)
 
 def main(n):
   u=[1.]*n
-  v=[0]*n
+  v=[0.]*n
+
   for i in range(10):
     eval_AtA_times_u(n,u,v)
     eval_AtA_times_u(n,v,u)
-  vBv=vv=0
-  for i in range(n): vBv+=u[i]*v[i]; vv+=v[i]*v[i]  
-  print("%.9f" % sqrt(vBv/vv))
+
+  vBv=vv=0.
+
+  for i in range(n):
+    vBv+=u[i]*v[i]; vv+=v[i]*v[i]  
+
+  #print("%.9f" % sqrt(vBv/vv))
+  print(f"{sqrt(vBv/vv):.9f}")
   
 #if __name__ == '__main__':
 #  main( int(sys.argv[1]) if len(sys.argv) > 1 else 100 )  
 
-main(500)
+main(200)
