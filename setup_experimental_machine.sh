8c758e235354ecd47f00f526a12b7466186cf2be
andrei-calin-dragomir <andrei.calin.dragomir@gmail.com>
2025-01-21 19:49:06 +0100

    Most recent setup

--- setup_experimental_machine.sh
+++ setup_experimental_machine.sh
@@ -3,11 +3,11 @@ cd EnergiBridge/
 sudo chgrp -R $USER /dev/cpu/*/msr
 sudo chmod g+r /dev/cpu/*/msr
 cargo build -r
-sudo setcap cap_sys_rawio=ep ./EnergiBridge/target/release/energibridge
+sudo setcap cap_sys_rawio=ep ./target/release/energibridge
 
 # Install Codon
 /bin/bash -c "$(curl -fsSL https://exaloop.io/install.sh)"
 
 # Install Pypy
-sudo apt install pypy-dev
+sudo apt install pypy3-dev
 
